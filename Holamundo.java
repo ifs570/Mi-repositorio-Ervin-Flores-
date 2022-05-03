a009e8e79689981c22ce627e7ad7461404f9c8de
Ervin Flores Sucapuca <efloressuc@unsa.edu.pe>
2022-05-03 00:07:27 -0500

    Bienvenidos a PW2

--- Holamundo.java
+++ Holamundo.java
@@ -2,5 +2,6 @@ public class Holamundo{
 public static void main(String args[]){
         System.out.println("Hola Mundo");
 	System.out.println("Mi primera Hola Mundo en git");	
+	System.out.println("Bienvenidos");
 }
 } 
